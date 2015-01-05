2ebe796a0979ba7d68685be91a883af5801026bc
Tim Jabaut <tjabaut@gmail.com>
2015-01-05 13:19:59 -0500

    Add files

---
+++ flask.py
@@ -0,0 +1,13 @@
+__author__ = 'tjabaut'
+
+from flask import render_template
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
