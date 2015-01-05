d412aea580106511b0191258e76ddb8b9c515369
Tim Jabaut <tjabaut@gmail.com>
2015-01-05 15:01:07 -0500

    Test Changes

--- testflask.py
+++ testflask.py
@@ -1,12 +1,14 @@
 __author__ = 'tjabaut'
 
-from flask import render_template
+from flask import Flask, render_template
+app = Flask(__name__)
+
+#from flask import render_template
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
-
 
 if __name__ == '__main__':
     app.debug = True
